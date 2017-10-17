ae92ced38c0df55ecfff6e4ff3e3901136388074
strix25 <damjan.oslaj@gmail.com>
2017-10-17 10:15:16 +0200

    added quote machine
    added quote machine

---
+++ proj/quoteMachine/js/index.js
@@ -0,0 +1,21 @@
+function randomQuote() {
+  $.ajax({
+      url: "https://api.forismatic.com/api/1.0/?",
+      dataType: "jsonp",
+      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+      success: function( response ) {
+        $("#randomQuote").html("<p id='randomQuote' class='lead'>" +
+          response.quoteText + "<br/>&dash; " + response.quoteAuthor + " </p>");
+        $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
+          ' (' + response.quoteAuthor + ')');
+      }
+  });
+}
+
+$(function() {
+  randomQuote();
+});
+
+$("button").click(function(){
+  randomQuote();
+});
